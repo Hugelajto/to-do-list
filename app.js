e5130416993227fa60eb461778a2a804696bc3c5
Hugelajto <huonghanna@gmail.com>
2024-06-02 23:56:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const formInput = document.querySelector(".form-input")
+const formBtn = document.querySelector(".form-button")
+const toDoList = document.querySelector(".to-do-list")
+
+
+const createLiElement = () => {
+    let li = document.createElement("li")
+    li.innerHTML = formInput.value
+    li.classList = "to-do-content"
+
+    
+    li.append(createDelBtn())
+    li.append(createEditBtn())
+
+    return li
+}
+
+const createDelBtn = () => {
+    let delBtn = document.createElement("button")
+    delBtn.classList = "delBtn"
+    delBtn.innerHTML = "X"
+    
+    delBtn.addEventListener("click", (e) => {
+        const del = e.target.parentElement
+        del.remove()
+    })
+    
+    return delBtn
+}
+
+const createEditBtn = () => {
+    let editBtn = document.createElement("button")
+    editBtn.classList = "editBtn"
+    editBtn.innerHTML = "edit"
+    
+    editBtn.addEventListener("click", (e) => {
+        const edit = e.target.parentElement
+        const input = document.createElement("input")
+        input.classList = "input"
+        edit.innerHTML = input.innerHTML
+        if (editBtn.className === "editBtn") {
+            editBtn.innerHTML = "save"
+        } else if (editBtn.className === "save") {
+            input.value = toDoList.innerHTML
+            input.classList.add("none")      
+            editBtn.innerHTML = "edit"
+            
+        }
+        edit.append(input, editBtn)
+        console.log(input)
+    })
+    
+    return editBtn
+}
+
+
+    const appendTodoList = () => {
+        if (formInput.value === ""){
+            alert ("Can't leave this empty!")
+        } else {
+            toDoList.append(createLiElement())
+        }
+    }
+
+formBtn.addEventListener("click", appendTodoList)
+
+toDoList.addEventListener("click", (ev) => {
+        ev.target.tagName === "li"
+        ev.target.classList.toggle("checked")
+}, false)
+
+
